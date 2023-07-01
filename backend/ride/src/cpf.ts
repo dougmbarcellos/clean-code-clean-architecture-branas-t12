154d5cc4f36fadafd8843e80e63da9187ad06538
Douglas M. Barcellos <douglasa321@gmail.com>
2023-07-01 20:50:07 -0300

    CPF: Criação de testes e refatoração do validate

--- backend/ride/src/cpf.ts
+++ backend/ride/src/cpf.ts
@@ -1,61 +1,50 @@
-// @ts-nocheck
-export function validate (str) {
+enum DigitType {
+  PENULTIMATE = 9, // 111.444.777-X5
+  LAST = 10, // 111.444.777-3X
+}
 
-	if (str !== null) {
-        if (str !== undefined) {
-            if (str.length >= 11 || str.length <= 14){
+function isRepeated(cpf: string): boolean {
+  const firstDigit = cpf.at(0);
 
-                str=str
-                    .replace('.','')
-                    .replace('.','')
-                    .replace('-','')
-                    .replace(" ","");  
-    
-                if (!str.split("").every(c => c === str[0])) {
-                    try{  
-                        let     d1, d2;  
-                        let     dg1, dg2, rest;  
-                        let     digito;  
-                            let     nDigResult;  
-                        d1 = d2 = 0;  
-                        dg1 = dg2 = rest = 0;  
-                            
-                        for (let nCount = 1; nCount < str.length -1; nCount++) {  
-                            // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-                            // 	return false;
-                            // } else {
-    
-                                digito = parseInt(str.substring(nCount -1, nCount));  							
-                                d1 = d1 + ( 11 - nCount ) * digito;  
-                
-                                d2 = d2 + ( 12 - nCount ) * digito;  
-                            // }
-                        };  
-                            
-                        rest = (d1 % 11);  
-                
-                        dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
-                        d2 += 2 * dg1;  
-                        rest = (d2 % 11);  
-                        if (rest < 2)  
-                            dg2 = 0;  
-                        else  
-                            dg2 = 11 - rest;  
-                
-                            let nDigVerific = str.substring(str.length-2, str.length);  
-                        nDigResult = "" + dg1 + "" + dg2;  
-                        return nDigVerific == nDigResult;
-                    }catch (e){  
-                        console.error("Erro !"+e);  
-    
-                        return false;  
-                    }  
-                } else return false
-    
-            }else return false;
-        }
+  return cpf.split('').every((digit) => digit === firstDigit);
+}
 
+const removeFormat = (cpf: string) => cpf.replace(/[^\d]/g, '');
 
-	} else return false;
+function getVerificationDigit(rawCPF: string, upToDigit: DigitType): string {
+  const reverseDigitList = rawCPF.slice(0, upToDigit).split('').reverse();
+  let multiplier = 2;
+  let sum = 0;
 
+  for (const digit of reverseDigitList) {
+    sum += parseInt(digit) * multiplier;
+    multiplier++;
+  }
+
+  const rest = sum % 11;
+  return String(rest <= 2 ? 0 : 11 - rest);
+}
+
+export function validate(formattedCPF: string): boolean {
+  if (!formattedCPF || formattedCPF.length < 11 || formattedCPF.length > 14)
+    return false;
+
+  const rawCPF = removeFormat(formattedCPF);
+  if (isRepeated(rawCPF)) return false;
+
+  try {
+    const penultimateDigit = getVerificationDigit(
+      rawCPF,
+      DigitType.PENULTIMATE
+    );
+    if (penultimateDigit !== rawCPF.at(DigitType.PENULTIMATE)) return false;
+
+    const lastDigit = getVerificationDigit(rawCPF, DigitType.LAST);
+    if (lastDigit !== rawCPF.at(DigitType.LAST)) return false;
+  } catch (e) {
+    console.error('Erro !' + e);
+    return false;
+  }
+
+  return true;
 }
