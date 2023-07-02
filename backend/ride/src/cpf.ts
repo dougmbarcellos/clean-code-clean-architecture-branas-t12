d524196938b9d69d337e8a8bb3c060c46a4e675c
Douglas M. Barcellos <douglasa321@gmail.com>
2023-07-01 21:10:51 -0300

    CPF: Ajustes na nomenclatura das funções.

--- backend/ride/src/cpf.ts
+++ backend/ride/src/cpf.ts
@@ -12,18 +12,16 @@ function hasSequentiallyRepeatedDigits(cpf: string): boolean {
   return cpf.split('').every((digit) => digit === firstDigit);
 }
 
-const cleanFormat = (cpf: string) => cpf.replace(/\D/g, '');
+const clearFormatting = (cpf: string) => cpf.replace(/\D/g, '');
 
 function getVerificationDigit(rawCPF: string, upToDigit: DigitType): string {
   const reverseDigitList = rawCPF.slice(0, upToDigit).split('').reverse();
   let multiplier = 2;
   let sum = 0;
-
   for (const digit of reverseDigitList) {
     sum += parseInt(digit) * multiplier;
     multiplier++;
   }
-
   const rest = sum % 11;
   return String(rest <= 2 ? 0 : 11 - rest);
 }
@@ -31,18 +29,18 @@ function getVerificationDigit(rawCPF: string, upToDigit: DigitType): string {
 export function validate(formattedCPF: string): boolean {
   if (!isValidLength(formattedCPF)) return false;
 
-  const rawCPF = cleanFormat(formattedCPF);
-  if (hasSequentiallyRepeatedDigits(rawCPF)) return false;
+  const cleanCPF = clearFormatting(formattedCPF);
+  if (hasSequentiallyRepeatedDigits(cleanCPF)) return false;
 
   try {
     const penultimateDigit = getVerificationDigit(
-      rawCPF,
+      cleanCPF,
       DigitType.PENULTIMATE
     );
-    if (penultimateDigit !== rawCPF.at(DigitType.PENULTIMATE)) return false;
+    if (penultimateDigit !== cleanCPF.at(DigitType.PENULTIMATE)) return false;
 
-    const lastDigit = getVerificationDigit(rawCPF, DigitType.LAST);
-    if (lastDigit !== rawCPF.at(DigitType.LAST)) return false;
+    const lastDigit = getVerificationDigit(cleanCPF, DigitType.LAST);
+    if (lastDigit !== cleanCPF.at(DigitType.LAST)) return false;
   } catch (e) {
     console.error(`Error ${e}!`);
     return false;
