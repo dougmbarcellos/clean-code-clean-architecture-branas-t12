591e367c30a636bb19f5478dda19cbbf37a5ad87
Douglas M. Barcellos <douglasa321@gmail.com>
2023-07-29 13:15:11 -0300

    Adicionada modelagem experimental no Passenger.

--- backend/ride/src/Passenger.ts
+++ backend/ride/src/Passenger.ts
@@ -1,9 +1,31 @@
+import { ObjectId } from 'mongodb';
 import CPFValidator from './CPFValidator';
 
-export default class Passenger {
-  passengerId: string = '';
+function modelId(idName: string) {
+  return function <T extends { new (...args: any[]): {} }>(constructor: T) {
+    return class extends constructor {
+      initModelId() {
+        const id = new ObjectId();
+        (this as any)._id = id;
+        (this as any)[idName] = id;
+      }
+    };
+  };
+}
+
+class Model {
+  constructor() {
+    this.initModelId();
+  }
+  initModelId() {}
+}
+
+@modelId('passengerId')
+export default class Passenger extends Model {
+  passengerId!: ObjectId;
 
   constructor(private name: string, private email: string, private document: string) {
+    super();
     this.validateRequired();
     new CPFValidator(this.document).validate();
   }
