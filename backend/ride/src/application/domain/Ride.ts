3875206eda906a0fa94042aa48c323cd71fa371c
Douglas M. Barcellos <douglasa321@gmail.com>
2023-08-16 16:06:18 -0300

    Apply Strategy Pattern in FareCalculator with Dynamic Static Factory.

--- backend/ride/src/application/domain/Ride.ts
+++ backend/ride/src/application/domain/Ride.ts
@@ -1,18 +1,14 @@
 import { ObjectId } from 'mongodb';
 import DistanceCalculator from './DistanceCalculator';
+import FareCalculatorFactory from './FareCalculatorFactory';
 import Position from './Position';
 import Segment from './Segment';
 import UUIDGenerator from './UUIDGenerator';
 
 export default class Ride {
-  OVERNIGHT_FARE = 3.9;
-  OVERNIGHT_SUNDAY_FARE = 5;
-  SUNDAY_FARE = 2.9;
-  NORMAL_FARE = 2.1;
-  MIN_PRICE = 10;
-
   segments: Segment[] = [];
   positions: Position[] = [];
+  MIN_PRICE = 10;
 
   constructor(
     readonly _id: ObjectId,
@@ -42,18 +38,8 @@ export default class Ride {
       if (!nextPosition) break;
       const distance = DistanceCalculator.calculate(position.coord, nextPosition.coord);
       const segment = new Segment(distance, nextPosition.date);
-      if (segment.isOvernight() && !segment.isSunday()) {
-        price += segment.distance * this.OVERNIGHT_FARE;
-      }
-      if (segment.isOvernight() && segment.isSunday()) {
-        price += segment.distance * this.OVERNIGHT_SUNDAY_FARE;
-      }
-      if (!segment.isOvernight() && segment.isSunday()) {
-        price += segment.distance * this.SUNDAY_FARE;
-      }
-      if (!segment.isOvernight() && !segment.isSunday()) {
-        price += segment.distance * this.NORMAL_FARE;
-      }
+      const fareCalculator = FareCalculatorFactory.create(segment);
+      price += fareCalculator.calculate(segment);
     }
     return price < this.MIN_PRICE ? this.MIN_PRICE : parseFloat(price.toFixed(2));
   }
