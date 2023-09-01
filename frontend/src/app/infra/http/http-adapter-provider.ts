696d7f32950e3c348c95aa9f12bdd8323389d30f
Douglas M. Barcellos <douglasa321@gmail.com>
2023-09-01 00:22:48 -0300

    Add apiInterceptor in provider declaration.

--- frontend/src/app/infra/http/http-adapter-provider.ts
+++ frontend/src/app/infra/http/http-adapter-provider.ts
@@ -1,11 +1,12 @@
-import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { EnvironmentProviders, Provider } from '@angular/core';
+import { apiInterceptor } from './api.interceptor';
 import { HttpClientService } from './http-client.service';
 
 export function provideHttpClientAdapter(): (Provider | EnvironmentProviders)[] {
   // { provide: HttpClientService, useClass: FetchAdapterService },
   return [
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([apiInterceptor])),
     {
       provide: HttpClientService,
       useClass: HttpClient,
