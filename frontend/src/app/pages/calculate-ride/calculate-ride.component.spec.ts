a6659fb39c4d29b1e65832f93298f4ff756cf19e
Douglas M. Barcellos <douglasa321@gmail.com>
2023-09-03 11:31:21 -0300

    Add new test to calculate-ride.

--- frontend/src/app/pages/calculate-ride/calculate-ride.component.spec.ts
+++ frontend/src/app/pages/calculate-ride/calculate-ride.component.spec.ts
@@ -6,16 +6,29 @@ import RidePositions from 'src/app/domain/ride-positions';
 import { provideHttpClientAdapterTesting } from 'src/app/infra/http/testing/http-adapter-provider-testing';
 import { CalculateRideComponent } from './calculate-ride.component';
 
+const date = '2013-03-01T01:10:00';
 const coordsSaoRoque = [-19.7392195, -40.6681334];
 const coordsSantaTeresa = [-19.9320348, -40.6102108];
 const coordsSaoRoqueCLAMAP = [-19.7392598, -40.6695496];
 
-describe.only('CalculateRideComponent', () => {
+describe('CalculateRideComponent', () => {
   let component: CalculateRideComponent;
   let fixture: ComponentFixture<CalculateRideComponent>;
   let httpTestingController: HttpTestingController;
 
+  function setInputValuesFromFormArray() {
+    const positions = component.rideBaseForm.positions;
+    positions.at(0).controls.lat.setValue(coordsSaoRoque[0]);
+    positions.at(0).controls.long.setValue(coordsSaoRoque[1]);
+    positions.at(0).controls.date.setValue(date);
+
+    positions.at(1).controls.lat.setValue(coordsSantaTeresa[0]);
+    positions.at(1).controls.long.setValue(coordsSantaTeresa[1]);
+    positions.at(1).controls.date.setValue(date);
+  }
+
   beforeEach(() => {
+    window.alert = jest.fn();
     TestBed.configureTestingModule({
       imports: [CalculateRideComponent],
       providers: [provideHttpClientAdapterTesting()],
@@ -31,17 +44,7 @@ describe.only('CalculateRideComponent', () => {
   });
 
   it('deve fazer o preenchimento dos campos e realizar a requisição para cálculo da corrida', () => {
-    window.alert = jest.fn();
-
-    const date = '2013-03-01T01:10:00';
-    const positions = component.rideBaseForm.positions;
-    positions.at(0).controls.lat.setValue(coordsSaoRoque[0]);
-    positions.at(0).controls.long.setValue(coordsSaoRoque[1]);
-    positions.at(0).controls.date.setValue(date);
-
-    positions.at(1).controls.lat.setValue(coordsSantaTeresa[0]);
-    positions.at(1).controls.long.setValue(coordsSantaTeresa[1]);
-    positions.at(1).controls.date.setValue(date);
+    setInputValuesFromFormArray();
 
     const btnCalculateDe = fixture.debugElement.query(By.css('#btnCalculate'));
     btnCalculateDe.triggerEventHandler('click');
@@ -63,11 +66,32 @@ describe.only('CalculateRideComponent', () => {
         },
       ],
     };
-
     expect(req.request.body).toEqual(requestBody);
 
     const responseBody = { price: 10 };
     req.flush(responseBody);
     expect(window.alert).toBeCalledWith(responseBody.price);
+  });
+
+  it('deve adicionar e preencher um novo percurso', () => {
+    setInputValuesFromFormArray();
+
+    const positions = component.rideBaseForm.positions;
+    const btnAddSegment = fixture.debugElement.query(By.css('#btnAddSegment'));
+    btnAddSegment.triggerEventHandler('click');
+    fixture.detectChanges();
+    const newSegmentIndex = 3;
+    expect(positions.length).toBe(newSegmentIndex);
+
+    const newSegmentFormGroup = positions.at(newSegmentIndex - 1);
+    newSegmentFormGroup.controls.lat.setValue(coordsSaoRoqueCLAMAP[0]);
+    newSegmentFormGroup.controls.long.setValue(coordsSaoRoqueCLAMAP[1]);
+    newSegmentFormGroup.controls.date.setValue(date);
+
+    const btnCalculateDe = fixture.debugElement.query(By.css('#btnCalculate'));
+    btnCalculateDe.triggerEventHandler('click');
+
+    const req = httpTestingController.expectOne('/calculate_ride');
+    expect(req.request.body.positions.length).toEqual(newSegmentIndex);
   });
 });
